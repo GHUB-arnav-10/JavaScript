b5530ddac74303a551d37117cbd11b7e1b5f3247
GHUB-arnav-10 <sudarnav2003@gmail.com>
2024-02-22 16:14:07 +0530

    Update app4.js
    This is how we should write code, to prevent sql injection.

--- app4.js
+++ app4.js
@@ -6,7 +6,9 @@ const {connection} = require("./db/db_config");
 
 app.get("/",(req,res)=>{
   let userInput = req.query.language
-  connection.query(`SELECT * FROM movie_data WHERE language = '${userInput}%'`,(err,results)=>{
+  connection.query(`SELECT * FROM movie_data WHERE language = ?`,
+  [userInput],
+  (err,results)=>{
     if(err){
       console.log(`Error in query!`)
       res.send("Error in DB query!");
