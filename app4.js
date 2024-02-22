f270dd4681cc7d3df129d1544ded9778473bd6c8
GHUB-arnav-10 <sudarnav2003@gmail.com>
2024-02-22 15:01:19 +0530

    Update app4.js

--- app4.js
+++ app4.js
@@ -4,6 +4,19 @@ const port = 3005;
 
 const {connection} = require("./db/db_config");
 
+app.get("/",(req,res)=>{
+  connection.query(`SELECT * FROM movie_data`,(err,results)=>{
+    if(err){
+      console.log(`Error in query!`)
+      res.send("Error in DB query!");
+    }
+    else{
+      console.log(results);
+      res.send(results);
+    }
+  })
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
